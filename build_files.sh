282f09536b1e62b7cf20740cba7a2ccc22597ca2
KATARI REVATHI <katari.revathi@gmail.com>
2024-06-21 22:05:58 +0530

    fourth

--- build_files.sh
+++ build_files.sh
@@ -13,11 +13,6 @@ export DJANGO_SETTINGS_MODULE=nucleus.settings
 # Collect static files
 python3.9 manage.py collectstatic --noinput
 
-# Ensure the public/static directory exists
-mkdir -p public/static
-
-# Move the collected static files to the public/static directory
-cp -r static/* public/static/
 
 # Apply database migrations
 python3.9 manage.py makemigrations
