fbbbd29683872f0f0f7d61cab210c3ac5174d23f
KATARI REVATHI <katari.revathi@gmail.com>
2024-06-28 17:23:28 +0530

    first21

--- nucleus/settings.py
+++ nucleus/settings.py
@@ -152,10 +152,8 @@ import os
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR / 'public/static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'static')
+STATICFILES_DIRS =  os.path.join(BASE_DIR,  'static'),
 
  
 MEDIA_URL = '/media/'
--- nucleus/urls.py
+++ nucleus/urls.py
@@ -30,5 +30,7 @@ urlpatterns = [
     path('',include('performance.urls')),
     path('accounts/', include('Accounts.urls')),
     path('', include('TUTORMODE.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  
