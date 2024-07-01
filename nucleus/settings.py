fc7676fb94da03b0b85112ace3065db4fd1dc11a
KATARI REVATHI <katari.revathi@gmail.com>
2024-07-01 13:46:33 +0530

    index page

--- nucleus/settings.py
+++ nucleus/settings.py
@@ -150,10 +150,14 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles' 'static')
-STATICFILES_DIRS =  os.path.join(BASE_DIR,  'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'public', 'static'),
+]
 
  
 MEDIA_URL = '/media/'
